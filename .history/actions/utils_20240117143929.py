6b6d752ea35f1e167d5bb47e4962b710268b69fd
shahzaibshah512 <shahzaibshah0028@gmail.com>
2024-01-17 20:50:29 +0500

    Initial commit

--- .history/actions/utils_20240117143929.py
+++
@@ -1,7 +0,0 @@
-from django.contrib.contenttypes import ContentType
-from .models import Action 
-
-def create_action(user, verb, target=None):
-    action = Action(user=user, verb=verb, target=target)
-    action.save()
-    
